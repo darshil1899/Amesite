3231ed69c32c7be147e42145f92e5f96281e30f8
Darshil Shah <darshilshah1899@gmail.com>
2023-08-06 18:47:55 -0400

    Moodle deploy
    Moodle deploy

--- elearning-backend/routes/grades.js
+++ elearning-backend/routes/grades.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Grade = require("../models/grades");
 const gradesController = require("../controllers/gradesController");
+const axios = require("axios");
 
 // Route to store the learner's grade data
 router.post("/store-grade", async (req, res) => {
@@ -15,6 +16,27 @@ router.post("/store-grade", async (req, res) => {
       assessment_id: assessment_id,
       grade: grade,
     });
+
+    // Push the grade data to Moodle using Moodle's REST API
+    const moodleBaseUrl = "https://your-moodle-domain.com"; // Replace with your Moodle domain
+    const moodleApiToken = "d7da8ce3e3fe295932e501f1fcde30fa"; // Replace with your Moodle API token
+    const moodleApiUrl = `${moodleBaseUrl}/webservice/rest/server.php`;
+    const moodleCourseId = 1; // Replace with the Moodle course ID where you want to record grades
+
+    // Modify this payload according to the Moodle REST API documentation
+    const moodlePayload = {
+      wstoken: moodleApiToken,
+      moodlewsrestformat: "json",
+      wsfunction: "Amesite",
+      courseid: moodleCourseId,
+      userid: user_id,
+      assessmentid: assessment_id,
+      grade: grade,
+    };
+
+    // Make a POST request to the Moodle API to add the grade
+    const response = await axios.post(moodleApiUrl, moodlePayload);
+    console.log("Moodle API response:", response);
 
     // Respond with the saved grade data (optional)
     res.status(201).json(savedGrade);
